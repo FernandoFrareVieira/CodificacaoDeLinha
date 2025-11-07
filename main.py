c37c262185e76cc46da0f47878480b07949183c7
Fernando <fernandofrarevieira@gmail.com>
2025-11-07 08:43:36 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import socket
+import json
+import tkinter
+import matplotlib
+
+def criarInterfaceGrafica():
+    print("Interface gráfica")
+
+def main():
+  print("TESTE")
+
+if __name__ == '__main__':
+  main()
